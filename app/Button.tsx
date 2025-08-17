fa71c53e1403f98fc371b1bc35f2e85f17cdecbf
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-08-17 14:11:47 +0200

    add random volume button

---
+++ app/Button.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { volumes } from "../lib/data";
+  
+export default function Button() {
+  const router = useRouter();
+
+  function handleClick() {
+    const volume = volumes[Math.floor(Math.random() * volumes.length)];
+    router.push(`/volumes/${volume.slug}`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me to go to a random volume
+    </button>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { introduction } from "@/lib/data";
 import Volumes from "./Volumes";
+import Button from "./Button";
 
 export default function Home() {
   return (
@@ -12,8 +13,9 @@ export default function Home() {
         <p>{ introduction }</p>
 
         <h2>All Volumes</h2>
-        < Volumes /> 
-      
+        < Volumes />
+        
+        < Button /> 
       </main>
       <footer className={styles.footer}>
         <a>
