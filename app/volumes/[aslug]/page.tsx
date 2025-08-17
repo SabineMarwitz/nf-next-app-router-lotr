e25659ff68a8daf064faaac8c0662f8079bdcb7f
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-08-17 12:17:21 +0200

    add detail page content

--- app/volumes/[aslug]/page.tsx
+++ app/volumes/[aslug]/page.tsx
@@ -1,3 +1,18 @@
-export default function Volume() {
-  return <h1>Volume Page</h1>;
+import { volumes } from "@/lib/data";
+import Link from "next/link";
+
+
+export default function VolumeDetail({ params }: { params: { aslug: string } }) {
+    const content = volumes.find(({slug}) => slug === params.aslug);
+
+    if(!content) {
+      return <>No Detail Page Content found</>
+    }
+
+    return <>
+        <Link href="/">All Volumes ...</Link>
+        <h1>{ content.title }</h1>
+        <p>{ content.description }</p>
+        <ul>{content.books.map((b) => <li key={b.ordinal}> {b.ordinal}: {b.title} </li> )}</ul>
+    </>
 }
